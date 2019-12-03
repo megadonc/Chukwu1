7f3fd1e0d7032f2593fe543578cb61651ef77b37
Chibueze <55157650+chibueze@users.noreply.github.com>
2019-12-03 14:44:28 +0100

    Added add method to the math class

---
+++ src/Math.java
@@ -0,0 +1,12 @@
+public class Math{
+
+    public static void main(String[] args) {
+        Math app = new Math();
+        int a =3;
+        int b = 4;
+        app.add(a,b);
+    }
+    public int add(int first, int second ){
+        return first + second;
+    }
+}
