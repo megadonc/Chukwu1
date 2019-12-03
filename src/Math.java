fc30079d83cadcf16794571e0ddbf1f1bf445a31
Chibueze <55157650+chibueze@users.noreply.github.com>
2019-12-03 14:46:46 +0100

    Added sub method to the math class

--- src/Math.java
+++ src/Math.java
@@ -5,8 +5,13 @@ public class Math{
         int a =3;
         int b = 4;
         app.add(a,b);
+        app.sub(a,b);
     }
     public int add(int first, int second ){
         return first + second;
+    }
+
+    public int sub(int first, int second ){
+        return first - second;
     }
 }
